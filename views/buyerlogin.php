713a5d4fce9a67d4c9ee0fe70cc2916f5f64fbc7
ashmyytaa <ashmi@live.ca>
2023-04-18 02:13:50 -0400

    Seller & Admin login layout

--- views/buyerlogin.php
+++ views/buyerlogin.php
@@ -1,3 +1,23 @@
-<?php
+<?php 
+namespace views;
+
 
 ?>
+<html>
+<body>
+
+<h1>Buyer Login</h1>
+<form action="" method="post">
+  <label for="username">username:</label><br>
+  <input type="text" id="username" name="username"><br>
+  <label for="password">password:</label><br>
+  <input type="password" id="password" name="password"><br><br>
+  <input type="submit" value="Login">
+</form>
+
+
+</body>
+</html>
+
+
+
--- views/sellerlogin.php
+++ views/sellerlogin.php
@@ -1,3 +1,20 @@
 <?php
-
+namespace views;
 ?>
+
+
+<html>
+<body>
+
+<h1>Seller Login</h1>
+<form action="" method="post">
+  <label for="username">username:</label><br>
+  <input type="text" id="username" name="username"><br>
+  <label for="password">password:</label><br>
+  <input type="password" id="password" name="password"><br><br>
+  <input type="submit" value="Login">
+</form>
+
+
+</body>
+</html>
